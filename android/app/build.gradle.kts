56c631d444b3a7e947ca75238ae5fc7d3a3b31b1
hein1225 <hein1225@gmail.com>
2026-03-04 14:04:22 +0800

    re

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,79 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "org.hein.heintv"
+    compileSdk = flutter.compileSdkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "org.hein.heintv"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    val keystorePropertiesFile = rootProject.file("key.properties")
+    val hasSigningConfig = keystorePropertiesFile.exists()
+
+    signingConfigs {
+        if (hasSigningConfig) {
+            create("release") {
+                val properties = Properties()
+                properties.load(FileInputStream(keystorePropertiesFile))
+                
+                storeFile = file(properties.getProperty("storeFile")!!)
+                storePassword = properties.getProperty("storePassword")
+                keyAlias = properties.getProperty("keyAlias")
+                keyPassword = properties.getProperty("keyPassword")
+            }
+        }
+    }
+
+    buildTypes {
+        release {
+            if (hasSigningConfig) {
+                signingConfig = signingConfigs.getByName("release")
+            } else {
+                // Fallback to debug signing for local development
+                signingConfig = signingConfigs.getByName("debug")
+            }
+            
+            // Enable R8 code shrinking, obfuscation, and optimization
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        
+        debug {
+            // Keep debug builds fast
+            isMinifyEnabled = false
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
---
+++ android/app/src/main/kotlin/org/hein/heintv/MainActivity.kt
@@ -0,0 +1,5 @@
+package org.hein.heintv
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
